e73415d3207123089a6cdf3937c1812b5a283e55
nessd <dness@dness.de>
2020-02-16 10:52:10 +0100

    course video 54 done

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,11 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Recipe } from '../recipe.model';
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.scss']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'Just a test', 'https://upload.wikimedia.org/wikipedia/commons/thumb/f/f4/Honeycrisp.jpg/480px-Honeycrisp.jpg'),
+    new Recipe('Another recipe', 'This is another fine food', 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/ba/Rice_grains_%28IRRI%29.jpg/640px-Rice_grains_%28IRRI%29.jpg')
+  ];
 
   constructor() { }
 
